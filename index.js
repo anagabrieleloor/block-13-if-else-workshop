e952d21d5d01c6e0ed778ac885f068f7594f6352
anagabrieleloor <137921244+anagabrieleloor@users.noreply.github.com>
2023-07-13 19:10:20 -0400

    completed workshop

---
+++ index.js
@@ -0,0 +1,262 @@
+// set variable for input value 
+//create output for if statement
+// output for else statement 
+
+/*
+let input = "I am a string";
+if (input !== "string") {
+    console.log ("true");
+} else {
+    console.log ("false")
+}*/
+
+/*
+let input = false;
+if (input == true) {
+    console.log ("true");
+} else {
+    console.log ("The boolean value false is falsy");
+}*/
+
+/*
+let input = null;
+if (input === undefined) {
+    console.log ("true");
+} else {
+    console.log ("The null value is falsy");
+}*/ 
+
+/*
+let input = undefined;
+if (input === null) {
+    console.log ("true");
+} else {
+    console.log ("undefined is falsy");
+}*/
+
+/*
+let input = 0;
+if (input === 1) {
+    console.log ("true");
+} else {
+    console.log ("The number 0 is falsy (the only falsy number)");
+}*/
+
+/*
+let input = "";
+if (input === "string") {
+    console.log ("true");
+} else {
+    console.log ("The empty string is falsy (the only falsy string)");
+}*/
+
+/*
+let num1 = 50;
+let num2 = 51;
+sum = 101;
+
+if (sum > 100) {
+    console.log (sum + " is greater than 100");
+} else if (sum < 100) {
+    console.log (sum + "is less than 100");
+}*/
+
+/*
+let num1 = 99;
+let num2 = -2;
+sum = 97;
+
+if (sum > 0) {
+    console.log (sum + " is greater than 0");
+} else if (sum < 0) {
+    console.log (sum + "is less than 0");
+}*/
+
+/*
+let num1 = 0;
+let num2 = 101;
+sum = 101;
+
+if (sum > 100) {
+    console.log (sum + " is greater than 100");
+} else if (sum < 100) {
+    console.log (sum + "is less than 100");
+}*/
+
+/*
+let num1 = 500;
+let num2 = -500;
+sum = 0;
+
+if (sum === 0) {
+    console.log (sum + " is equal to 0");
+} else if (sum > 0) {
+    console.log (sum + "is greater than 0");
+}*/
+
+/*
+let num1 = -1000;
+let num2 = 0;
+sum = -1000;
+
+if (sum < 0) {
+    console.log (sum + " is a negative number");
+} else if (sum > 0) {
+    console.log (sum + "is greater than 0");
+}*/
+
+/*
+let num1 = -5;
+let num2 = 0;
+sum = -5;
+
+if (sum < 0) {
+    console.log (sum + " is a negative number");
+} else if (sum > 0) {
+    console.log (sum + "is greater than 0");
+}*/
+
+/*
+let num1 = 5;
+let num2 = 6;
+sum = 11;
+
+if (sum >= 5) {
+    console.log ("true");
+} else if (sum > 5) {
+    console.log ("false");
+}*/
+
+/*
+let num1 = 10;
+let num2 = 11;
+
+
+if (num1 && num2 >= 5) {
+    console.log ("true");
+} else if (num1 || num2 < 5) {
+    console.log ("false");
+}
+*/
+
+/*
+let num1 = 0;
+let num2 = 0;
+
+
+if (num1 && num2 >= 5) {
+    console.log ("true");
+} else if (num1 || num2 < 5) {
+    console.log ("false");
+}
+*/
+
+/*
+let num1 = 1000;
+let num2 = -1000;
+
+
+if (num1 && num2 >= 5) {
+    console.log ("true");
+} else if (num1 || num2 < 5) {
+    console.log ("false");
+}
+*/
+
+/*
+let num1 = 6;
+let num2 = 4;
+
+
+if (num1 && num2 >= 5) {
+    console.log ("true");
+} else if (num1 || num2 < 5) {
+    console.log ("false");
+}
+*/
+
+/*
+let num1 = 5;
+let num2 = 5;
+
+
+if (num1 && num2 >= 5) {
+    console.log ("true");
+} else if (num1 || num2 < 5) {
+    console.log ("false");
+}
+*/
+
+/*
+param1A = "cat";
+param1B = "cat";
+param2A = 6;
+param2B = "6";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+*/
+/*
+param1A = "five";
+param1B = 5;
+param2A = "dog";
+param2B = "dawg";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+*/
+/*
+param1A = 0;
+param1B = false;
+param2A = "horse";
+param2B = "horse";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+*/
+
+/*
+param1A = "eight";
+param1B = "eight";
+param2A = "ate";
+param2B = "ate";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+*/
+/*
+param1A = "11";
+param1B = "eleven";
+param2A = "four";
+param2B = "for";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+*/
+
+param1A = "cake";
+param1B = "cake";
+param2A = "pie";
+param2B = "pie";
+
+if (param1A === param1B || param2A === param2B) {
+    console.log ("true");
+} else if (param1A != param1B && param1A != param2B) {
+    console.log ("false");
+}
+
